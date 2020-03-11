963aa407c1d07716ce22b63b5efbffd74ef7c155
Enoc-Martinez <shanock.charras@gmail.com>
2020-03-11 12:38:22 -0600

    Componente principal y cabecero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Ingreso } from './ingreso/Ingreso.model';
+import { Egreso } from './egreso/Egreso.model';
+import { IngresoServicio } from './ingreso/Ingreso.service';
+import { EgresoServicio } from './egreso/Egreso.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'presupuesto-app';
+  ingresos: Ingreso[] = [];
+  egresos: Egreso[] = [];
+
+  constructor(
+    private ingresoService: IngresoServicio,
+    private egresoService: EgresoServicio
+  ) {
+    this.ingresos = this.ingresoService.ingresos;
+    this.egresos = this.egresoService.egresos;
+  }
+
+  getIngresoTotal() {
+    let ingresoTotal = 0;
+    this.ingresos.forEach(item => ingresoTotal += item.valor);
+    return ingresoTotal;
+  }
+
+  getEgresoTotal() {
+    let egresoTotal = 0;
+    this.egresos.forEach(item => egresoTotal += item.valor);
+    return egresoTotal;
+  }
+
+  getPorcentajeTotal() {
+    return this.getEgresoTotal() / this.getIngresoTotal();
+  }
+
+  getPresupuestoTotal() {
+    return this.getIngresoTotal() - this.getEgresoTotal();
+  }
 }
--- src/app/cabecero/cabecero.component.ts
+++ src/app/cabecero/cabecero.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cabecero',
   templateUrl: './cabecero.component.html',
   styleUrls: ['./cabecero.component.css']
 })
-export class CabeceroComponent implements OnInit {
+export class CabeceroComponent {
+  @Input() presupuestoTotal: number;
+  @Input() ingresoTotal: number;
+  @Input() egresoTotal: number;
+  @Input() porcentajeTotal: number;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
